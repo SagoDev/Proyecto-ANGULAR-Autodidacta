a51a8b8175512d3144a6aa3d1ea2d895f4ec7b26
Santiago Lanz <Santiagolanz019@gmail.com>
2024-05-30 18:24:29 -0300

    Style Footer Component
    - Added some CSS to the footer component.

--- client/src/app/pages/public/welcome-page/welcome-page.component.ts
+++ client/src/app/pages/public/welcome-page/welcome-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss'
 })
