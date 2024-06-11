b84c5c31d9be2f3b6fec1fad80615ed39703e727
Santiago Lanz <Santiagolanz019@gmail.com>
2024-06-11 14:59:41 -0300

    Style Private components
    - Added some html, css to te private components.
    - Created shared side-menu component.
    - Changed html, css to navbar component.

--- client/src/app/pages/private/dashboard/dashboard.component.ts
+++ client/src/app/pages/private/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { UserOptionsComponent } from '../user-options/user-options.component';
+import { FolderComponent } from '../folder/folder.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SideMenuComponent, NavbarComponent, UserOptionsComponent, FolderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
