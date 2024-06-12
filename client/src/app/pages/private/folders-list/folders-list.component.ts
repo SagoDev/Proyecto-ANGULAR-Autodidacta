bec24b05e25b686992021a4a8a0c3ea93efaa769
Santiago Lanz <Santiagolanz019@gmail.com>
2024-06-12 13:33:54 -0300

    Folders list component
    - Added folders list component.

---
+++ client/src/app/pages/private/folders-list/folders-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-folders-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './folders-list.component.html',
+  styleUrl: './folders-list.component.scss'
+})
+export class FoldersListComponent {
+
+}
