ff3da9a58d8c0cdec50f0c4899baf4635fc0c097
Santiago Lanz <Santiagolanz019@gmail.com>
2024-06-14 15:29:14 -0300

    Deleted Components
    - Deleted  user-options component.
    - Deleted  add-folder component.
    - Deleted  dashboard component.
    - Deleted  navbar component.

--- client/src/app/pages/private/private.routes.ts
+++ client/src/app/pages/private/private.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../public/auth/login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const PRIVATE_ROUTES: Routes = [
     {
         path: '', component: LoginComponent,
         title: 'Login'
-    },
-    {
-        path:'dashboard', component:DashboardComponent,
-        title:'Dashboard'
-    }
+    }    
 ];
